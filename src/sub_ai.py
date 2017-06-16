bc3cc889e3babf08b4e5fdb8f93c3cf64e01a8dc
[Vinh Nguyen] <[vnguye34@students.kennesaw.edu]>
2017-06-16 00:09:47 -0400

    subscriber callback not changing global variable recieved

--- src/sub_ai.py
+++ src/sub_ai.py
@@ -5,53 +5,69 @@ import rospy
 import smach
 import smach_ros
 from std_msgs.msg import String
+recieved = '0'
 
 # define Start Gate
 class start_gate(smach.State):
+
     def __init__(self):
         smach.State.__init__(self, outcomes=['start_gate_complete', 'start_gate_null'])
-        self.pub = rospy.Publisher('start_gate_smach_pub', String, queue_size='10')
-        rate = rospy.Rate(10)
+        #self.pub = rospy.Publisher('start_gate_smach_pub', String, queue_size='10')
+        #rate = rospy.Rate(.5)
+        #self.sub = rospy.Subscriber("start_gate_smach_sub", String, self.callback)
+        #rospy.spin()
 
-    def callback(data):
-        rospy.loginfo(rospy.get_caller_id(), data.data)
-        recieved = data.data 
+    def callback(self, data):
+        rospy.loginfo(rospy.get_caller_id() + ' %s', data.data)
+        recieved = data.data
+        rospy.loginfo('callback Display data.data: %s', recieved)
+        rospy.spin()
 
-    def start_gate_smach_sub():
-        rospy.init_node('start_gate_smach_sub', anonymous =True)
-        self.sub = rospy.Subscriber("start_gate_smach_sub", String, callback)
+    def execute(self, data):
+        #rospy.loginfo('Executing state start_gate')
+        #start_gate_String_pub = 'run_start_gate'
+        #self.pub.publish(start_gate_String_pub)
+        self.sub = rospy.S
+        subscriber("start_gate_smach_sub", String, self.callback)
 
-    def execute(self, userdata):
-        rospy.loginfo('Executing state start_gate')
-        start_gate_string_pub = 'run_start_gate'
-        self.pub.publish(start_gate_string_pub)
-        if recieved = 'start_gate_complete'
-            rospy.infolog(data.data)
-            return 'start_gate_complete'
-        else:
+        if recieved == '0':
+            self.sub = rospy.Subscriber("start_gate_smach_sub", String, self.callback)
+            rospy.loginfo('execute data: %s', recieved)
             return 'start_gate_null'
+            rospy.spin()
+        else: 
+            if recieved == '1':
+                return 'start_gate_complete'
+            else:
+                return 'start_gate_null'
+        rospy.spin()
 
 #define bouys
 class bouys(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['bouy_complete', 'bouy_null'])
-        self.pub = rospy.Publisher('bouy_smach_publisher', String, queue_size='10')
-        self.sub = rospy.Subscriber("bouy_smach_pub", String, callback)
+        self.pub = rospy.Publisher('bouy_smach_pub', String, queue_size='10')
         rate = rospy.Rate(10)
     
-    def callback(data)
+    def callback(self, data):
         rospy.loginfo(rospy.get_caller_id(), data.data)
+        recieved = data.data
+
+    def bouy_smach_sub():
+        rospy.init_node('bouy_smach_sub', anonymous =True)
+        self.sub = rospy.Subscriber("bouy_smach_sub", String, callback)
 
     def execute(self, userdata):
-        rospy.loginfo('Executing state bouy')   
-        bouy_string_pub = 'run_bouy'
-        self.pub.publish(bouy_string_pub)
-        if data.data = 'bouy_complete'
+        rospy.loginfo('Executing state bouy')
+        bouy_String_pub = 'run_bouy'
+        self.pub.publish(bouy_String_pub)
+        rospy.loginfo('running bouys, waiting for confirmation')
+        if data.data == 'bouy_complete':
             return 'bouy_complete'
         else:
             return 'bouy_null'
 
-def main()
+def main():
     rospy.init_node('sub_ai_state_manager')
 
     #create SMACH state machine
@@ -60,16 +76,16 @@ def main()
     #Open Container
     with sm:
         #add states to the Container
-        smach.StateMachine.add('start_gate', start_gate())
-                                transitions={'start_gate_complete':'bouy_null'}
-        smach.StateMachine.add('bouys', bouys())
-                                transitions={'bouy_complete':'celebrate'}
+        smach.StateMachine.add('start_gate', start_gate(),
+                            transitions={'start_gate_complete':'celebrate', 'start_gate_null':'start_gate'})
+        smach.StateMachine.add('bouys', bouys(),
+                            transitions={'bouy_complete':'celebrate', 'bouy_null':'bouys'})
     #create Introspection server to view
     sis = smach_ros.IntrospectionServer('smach_server', sm, '/SM_ROOT')
-    sis.start_gate
+    sis.start()
     
     #Execute SMACH plan
-    sm.execute
+    outcome = sm.execute()
 
     #Tell ROS to go around again
     rospy.spin()
