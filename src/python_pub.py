2a701b2bb7f380204583ca5ef879bb3119655e69
[Vinh Nguyen] <[vnguye34@students.kennesaw.edu]>
2017-06-16 00:07:45 -0400

    change publisher to string '1' for sub_ai

--- src/python_pub.py
+++ src/python_pub.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import String 
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    #pub = rospy.Publisher('chatter', String, queue_size=10)
+    #pub = rospy.Publisher('01_start_gate_smach_sub', String, queue_size=10)
+    pub = rospy.Publisher('start_gate_smach_sub', String, queue_size=10)
+    rospy.init_node('data_publisher', anonymous=False)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        #hello_str = "hello world %s" % rospy.get_time()
+	#hello_str = "start_gate_complete"
+	#hello_str = 1
+	derp = '1'
+        rospy.loginfo(derp)
+        pub.publish(derp)
         rate.sleep()
 
 if __name__ == '__main__':
