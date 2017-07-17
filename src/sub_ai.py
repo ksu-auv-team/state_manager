c94496397962ebff362299ffdc12b91d0c6ad9cd
[Vinh Nguyen] <[vnguye34@students.kennesaw.edu]>
2017-07-17 19:28:35 -0400

    modify publisher/subscriber topic names

--- src/sub_ai.py
+++ src/sub_ai.py
@@ -15,7 +15,7 @@ class start_gate(smach.State):
         global recieved
         recieved = '0'
         smach.State.__init__(self, outcomes=['start_gate_complete', 'start_gate_null'])
-        self.pub = rospy.Publisher('start_gate_smach_pub', String, queue_size='10')
+        self.pub = rospy.Publisher('start_gate_smach_transmitter', String, queue_size='10')
         rate = rospy.Rate(.5)
 
     def callback(self, data):
@@ -31,7 +31,7 @@ class start_gate(smach.State):
         rospy.loginfo('Executing state start_gate')
         start_gate_String_pub = 'run_start_gate'
         self.pub.publish(start_gate_String_pub)
-        self.sub = rospy.Subscriber("start_gate_smach_sub", String, self.callback)
+        self.sub = rospy.Subscriber("start_gate_smach_receiver", String, self.callback)
         global recieved
         rospy.loginfo('execute data: %s', recieved)
 
@@ -50,7 +50,7 @@ class path_marker_1(smach.State):
 
     def __init__(self):
         smach.State.__init__(self, outcomes=['path_marker_1_complete', 'path_marker_1_null'])
-        self.pub = rospy.Publisher('path_marker_1_smach_pub', String, queue_size='10')
+        self.pub = rospy.Publisher('path_marker_1_smach_transmitter', String, queue_size='10')
         rate = rospy.Rate(.5)
 
     def callback(self, data):
@@ -66,7 +66,7 @@ class path_marker_1(smach.State):
         rospy.loginfo('Executing state path_marker_1')
         start_gate_String_pub = 'run_path_marker_1'
         self.pub.publish(path_marker_1_String_pub)
-        self.sub = rospy.Subscriber("path_marker_1_smach_sub", String, self.callback)
+        self.sub = rospy.Subscriber("path_marker_1_smach_receiver", String, self.callback)
         global recieved
         rospy.loginfo('execute data: %s', recieved)
 
@@ -85,7 +85,7 @@ class bouys(smach.State):
 
     def __init__(self):
         smach.State.__init__(self, outcomes=['bouys_complete', 'bouys_null'])
-        self.pub = rospy.Publisher('bouys_smach_pub', String, queue_size='10')
+        self.pub = rospy.Publisher('bouys_smach_transmitter', String, queue_size='10')
         rate = rospy.Rate(.5)
 
     def callback(self, data):
@@ -101,7 +101,7 @@ class bouys(smach.State):
         rospy.loginfo('Executing state bouys')
         start_gate_String_pub = 'run_bouys'
         self.pub.publish(bouys_String_pub)
-        self.sub = rospy.Subscriber("bouys_smach_sub", String, self.callback)
+        self.sub = rospy.Subscriber("bouys_smach_receiver", String, self.callback)
         global recieved
         rospy.loginfo('execute data: %s', recieved)
 
@@ -120,7 +120,7 @@ class path_marker_2(smach.State):
 
     def __init__(self):
         smach.State.__init__(self, outcomes=['path_marker_2_complete', 'path_marker_2_null'])
-        self.pub = rospy.Publisher('path_marker_2_smach_pub', String, queue_size='10')
+        self.pub = rospy.Publisher('path_marker_2_smach_transmitter', String, queue_size='10')
         rate = rospy.Rate(.5)
 
     def callback(self, data):
@@ -155,7 +155,7 @@ class channel(smach.State):
 
     def __init__(self):
         smach.State.__init__(self, outcomes=['channel_complete', 'channel_null'])
-        self.pub = rospy.Publisher('channel_smach_pub', String, queue_size='10')
+        self.pub = rospy.Publisher('channel_smach_transmitter', String, queue_size='10')
         rate = rospy.Rate(.5)
 
     def callback(self, data):
@@ -171,7 +171,7 @@ class channel(smach.State):
         rospy.loginfo('Executing state path_marker_2')
         start_gate_String_pub = 'run_channel'
         self.pub.publish(channel_String_pub)
-        self.sub = rospy.Subscriber("channel_smach_sub", String, self.callback)
+        self.sub = rospy.Subscriber("channel_smach_receiver", String, self.callback)
         global recieved
         rospy.loginfo('execute data: %s', recieved)
 
@@ -190,7 +190,7 @@ class hydrophone_1(smach.State):
 
     def __init__(self):
         smach.State.__init__(self, outcomes=['hydrophone_1_complete', 'hydrophone_1_null'])
-        self.pub = rospy.Publisher('hydrophone_1_smach_pub', String, queue_size='10')
+        self.pub = rospy.Publisher('hydrophone_1_smach_transmitter', String, queue_size='10')
         rate = rospy.Rate(.5)
 
     def callback(self, data):
@@ -206,7 +206,7 @@ class hydrophone_1(smach.State):
         rospy.loginfo('Executing state hydophone_1')
         start_gate_String_pub = 'run_hydophone_1'
         self.pub.publish(hydrophone_1_String_pub)
-        self.sub = rospy.Subscriber("hydrophone_1_smach_sub", String, self.callback)
+        self.sub = rospy.Subscriber("hydrophone_1_smach_receiver", String, self.callback)
         global recieved
         rospy.loginfo('execute data: %s', recieved)
 
@@ -225,7 +225,7 @@ class dropper(smach.State):
 
     def __init__(self):
         smach.State.__init__(self, outcomes=['dropper_complete', 'dropper_null'])
-        self.pub = rospy.Publisher('dropper_smach_pub', String, queue_size='10')
+        self.pub = rospy.Publisher('dropper_smach_transmitter', String, queue_size='10')
         rate = rospy.Rate(.5)
 
     def callback(self, data):
@@ -241,7 +241,7 @@ class dropper(smach.State):
         rospy.loginfo('Executing state dropper')
         start_gate_String_pub = 'run_dropper'
         self.pub.publish(dropper_String_pub)
-        self.sub = rospy.Subscriber("dropper_smach_sub", String, self.callback)
+        self.sub = rospy.Subscriber("dropper_smach_receiver", String, self.callback)
         global recieved
         rospy.loginfo('execute data: %s', recieved)
 
@@ -260,7 +260,7 @@ class torpedo(smach.State):
 
     def __init__(self):
         smach.State.__init__(self, outcomes=['torpedo_complete', 'torpedo_null'])
-        self.pub = rospy.Publisher('torpedo_smach_pub', String, queue_size='10')
+        self.pub = rospy.Publisher('torpedo_smach_transmitter', String, queue_size='10')
         rate = rospy.Rate(.5)
 
     def callback(self, data):
@@ -276,7 +276,7 @@ class torpedo(smach.State):
         rospy.loginfo('Executing state path_marker_2')
         start_gate_String_pub = 'run_torpedo'
         self.pub.publish(torpedo_String_pub)
-        self.sub = rospy.Subscriber("torpedo_smach_sub", String, self.callback)
+        self.sub = rospy.Subscriber("torpedo_smach_receiver", String, self.callback)
         global recieved
         rospy.loginfo('execute data: %s', recieved)
 
@@ -295,7 +295,7 @@ class hydrophone_2(smach.State):
 
     def __init__(self):
         smach.State.__init__(self, outcomes=['hydrophone_2_complete', 'hydrophone_2_null'])
-        self.pub = rospy.Publisher('hydrophone_2_smach_pub', String, queue_size='10')
+        self.pub = rospy.Publisher('hydrophone_2_smach_transmitter', String, queue_size='10')
         rate = rospy.Rate(.5)
 
     def callback(self, data):
@@ -311,7 +311,7 @@ class hydrophone_2(smach.State):
         rospy.loginfo('Executing state path_marker_2')
         start_gate_String_pub = 'run_hydrophone_2'
         self.pub.publish(hydrophone_2_String_pub)
-        self.sub = rospy.Subscriber("hydrophone_2_smach_sub", String, self.callback)
+        self.sub = rospy.Subscriber("hydrophone_2_smach_receiver", String, self.callback)
         global recieved
         rospy.loginfo('execute data: %s', recieved)
 
@@ -330,7 +330,7 @@ class samples(smach.State):
 
     def __init__(self):
         smach.State.__init__(self, outcomes=['samples_complete', 'samples_null'])
-        self.pub = rospy.Publisher('samples_smach_pub', String, queue_size='10')
+        self.pub = rospy.Publisher('samples_smach_transmitter', String, queue_size='10')
         rate = rospy.Rate(.5)
 
     def callback(self, data):
@@ -346,7 +346,7 @@ class samples(smach.State):
         rospy.loginfo('Executing state samples')
         start_gate_String_pub = 'run_samples'
         self.pub.publish(samples_String_pub)
-        self.sub = rospy.Subscriber("samples_smach_sub", String, self.callback)
+        self.sub = rospy.Subscriber("samples_smach_receiver", String, self.callback)
         global recieved
         rospy.loginfo('execute data: %s', recieved)
 
